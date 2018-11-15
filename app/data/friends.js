743cbb8a0fbf3fa9cb479ba3d7c9934d4daf9609
KidCosa <imotley91@gmail.com>
2018-11-15 17:54:00 -0500

    Everything should work now!

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendArray = [
+    {
+        name: "Isaac",
+        photo: "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/42314507_1901808099913147_6224512221657956352_n.jpg?_nc_cat=111&_nc_ht=scontent.fatl1-1.fna&oh=facdca1232c64f2b7bc9a987cd2b218f&oe=5C681202",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Steve",
+        photo: "https://static.parade.com/wp-content/uploads/2017/09/montel-williams-new-series-ftr.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+]
+
+module.exports = friendArray;
